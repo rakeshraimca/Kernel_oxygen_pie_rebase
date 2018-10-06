3ed0f27ae281b289a031ef18ea01066782ef3d88
Rakesh Roshan <rakeshraimca@gmail.com>
2018-10-07 02:30:26 +0530

    adding ondemandX governor

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -616,6 +616,10 @@ extern struct cpufreq_governor cpufreq_gov_electron_advance;
 extern struct cpufreq_governor cpufreq_gov_ironactive;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_ironactive)
 
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_ONDEMANDX)
+extern struct cpufreq_governor cpufreq_gov_ondemandx;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_ondemandx)
+
 #endif
 
 /*********************************************************************
